f55c6fcc466d9e4b55c0698d0528fa6179f5b2b2
Jason Walsh <jwalsh@saylent.com>
2017-06-13 20:37:51 -0400

    Validating notes

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var bitcoin = require('bitcoin');
+// console.log(Object.keys(bitcoin));
+
+var crypto = require('crypto');
+// console.log(Object.keys(crypto));
+
 //these are some configurations that will need adjusting based on the configuration of your bitcoin.conf file
 var client = new bitcoin.Client({
   host: 'localhost',
@@ -7,8 +12,6 @@ var client = new bitcoin.Client({
   pass: 'pass'
 });
 //change the address to the address stored on your bitcoin core instance
-
-let config = {}
 
 var address = "mneUFSS4hz8deEfQqXypQ3Ho36tASNhirL"
 var message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sodales augue ut commodo tempus. Nullam porta libero vel urna viverra hendrerit. "
@@ -24,10 +27,12 @@ client.signMessage(address, message, function(err, signature){
     if (err){
       return console.error(err);
     }
-    console.log(address)
-    console.log(message)
-    console.log(signature1)
-    console.log(boolVal)
+    console.log('address', address)
+    console.log('message', message)
+    console.log('signature', signature)
+    console.log('verified', boolVal)
+
+    console.log(`~/opt/bitcoin/bin/bitcoin-cli verifymessage "${address}" "${signature}" "${message}"`)
 
   });
   console.log("signature "+signature)
